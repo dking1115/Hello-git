75496d3a2ad2f3edb6242954858673488a3c86ec
dking1115 <davidking@amdg.rockhursths.edu>
2020-02-04 08:38:59 -0600

    added hellogit

---
+++ HelloGit.py
@@ -0,0 +1,43 @@
+print("Hello, World!")
+a=6
+b=5
+print(a+b)
+
+mylist=[1,2,3,4,3,2,1]
+
+print(mylist[0:6:2])
+
+phrase='Catch the dog'
+phrase[2] # t
+phrase[4] # h
+phrase[-1:0:-3]
+
+
+def sumFunction(a,b):
+	return(a+b)
+
+print(sumFunction(2,20))
+
+x=10
+y=-10
+
+if(x==10):
+	x=5
+elif(y==-10):
+	y=5
+else:
+	x=y-x
+mylist=[1,3,8,412,43,2,20]
+
+len(mylist)
+for i in range(len(mylist)):
+	print(mylist[i])
+
+for val in mylist:
+	print(val)
+
+lookup={}
+lookup['kc']='chiefs'
+lookup['ne']='patriots'
+lookup['la']='chargers'
+lookup['suck']='raiders'
