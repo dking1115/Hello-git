56c934688aba761459d678e51cad6d43931717ad
dking1115 <davidking@amdg.rockhursths.edu>
2020-02-10 08:21:02 -0600

    added myio.py

---
+++ myio.py
@@ -0,0 +1,16 @@
+phrase =input("enter a string")
+
+print("you said"+phrase)
+
+print(f"you said {phrase}")
+
+
+somefloat=float(input("Enter a float: "))
+print("you entered float: "+str(somefloat))
+print(f"you entered float: {somefloat}")
+
+someint=float(input("enter an int: "))
+print("you entered int:"+str(someint))
+print(f"you entered int: {someint}")
+
+print(f"do python inline, like this: {somefloat}*{someint}={somefloat * someint}")
